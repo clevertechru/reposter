2efaea117fe6af22ea5df555c2871313cd91bbb6
Vitali Makarov <master@clevertech.ru>
2014-02-08 19:19:02 +0400

    fix layouts and avatar_url helper

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140208104009) do
+ActiveRecord::Schema.define(:version => 20140208145325) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20140208104009) do
     t.string   "uid"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "avatar_url"
   end
 
   create_table "users_roles", :id => false, :force => true do |t|
