7c4a173ed5a2a661949c7c7e4cb612cdc2390d6a
Vitali Makarov <master@clevertech.ru>
2014-04-05 01:34:48 +0400

    add guest icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
     if user.avatar_url.present?
       user.avatar_url
     elsif user.email.present?
-      default_url = "#{root_url}images/guest_32x32.png"
+      default_url = "#{root_url}assets/guest_32x32.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
     else
-      "#{root_url}images/guest_32x32.png"
+      "#{root_url}assets/guest_32x32.png"
     end
   end
 end
